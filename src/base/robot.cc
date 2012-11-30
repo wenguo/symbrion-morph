02ef662ad3645936fd83b9349e5cfed507ff92c9
liuwenguo <liuwenguo@gmail.com>
2012-11-30 16:46:38 +0000

    added irobot path for version 4757, board D on scouts waits for quite long time to get ID

--- src/base/robot.cc
+++ src/base/robot.cc
@@ -216,7 +216,7 @@ bool Robot::Init(const char * optionfile)
     current_state = fsm_state_t(para.init_state);
     last_state = fsm_state_t(para.init_state);
 
-    //SPIVerbose = QUIET;
+    SPIVerbose = QUIET;
 
     InitLog();
 
