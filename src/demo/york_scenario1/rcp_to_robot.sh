26088a2e50e1441b86d2b200fec7a8b678b36670
liuwenguo <liuwenguo@gmail.com>
2013-06-25 15:05:04 +0000

    added demo binaries

---
+++ src/demo/york_scenario1/rcp_to_robot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+	echo "usage: rcp2robot.sh NR"
+	exit 1
+fi
+
+FILES="aw_option.cfg \
+       scout_option.cfg\
+       kit_option.cfg"
+counter=0
+for f in $FILES
+do
+    if [ -f $f.${1} ]; then
+        echo "  " $f.${1} " --> 192.168.52.${1}/flash/morph/$f"
+        rcp  $f.${1} root@192.168.52.${1}:/flash/morph/$f
+    #else
+    #    echo $f.${1} "is not found"
+    fi
+done
