b7102c19dac0a853ec6bc940f438099a99cf1e53
liuwenguo <liuwenguo@a568fb57-e411-f16f-9b0f-bf232ce77aa6>
2013-09-20 11:48:18 +0000

    demo2

--- gui/canvas.cc
+++ gui/canvas.cc
@@ -91,30 +91,41 @@ void Canvas::draw_overlay()
                 {
                     blob_rect * blob = &(imagebox->blob_info[i].blobs[j]);
                     fl_color(colors[i]);
-                    fl_rect(blob->offset.x -  blob->size.x / 2 + orig_x,
-                            - blob->offset.y - blob->size.y / 2 + orig_y,
-                            blob->size.x,
-                            blob->size.y);
-                    fl_color(FL_WHITE);
+                    if(imagebox->flipped_image)
+                    {
+                        fl_rect(-blob->offset.x -  blob->size.x / 2 + orig_x,
+                                 blob->offset.y - blob->size.y / 2 + orig_y,
+                                blob->size.x,
+                                blob->size.y);
+                        fl_color(FL_WHITE);
 
-                    /*
-                    printf("draw %d on (%d %d) size (%d %d)\n", blob->id,
-                            blob->offset.x -  blob->size.x / 2 + orig_x,
-                            -blob->offset.y - blob->size.y / 2 + orig_y,
-                            blob->size.x,
-                            blob->size.y);*/
-                    /*fl_rect(blob->offset.x + startx - 2,
-                      blob->offset.y + starty - 2,
-                      4,
-                      4);*/
-                    fl_line(blob->offset.x -10 + orig_x, -blob->offset.y + orig_y,
-                            blob->offset.x + 10 + orig_x, -blob->offset.y + orig_y);
-                    fl_line(blob->offset.x + orig_x, -blob->offset.y - 10 + orig_y,
-                            blob->offset.x+ orig_x, -blob->offset.y + 10 + orig_y);
-                    sprintf(str, "%d", blob->id);
-                    fl_color(FL_YELLOW);
-                    fl_draw(str,blob->offset.x + orig_x, -blob->offset.y + orig_y);
-                    fl_color(FL_BLACK);
+                        fl_line(-blob->offset.x -10 + orig_x, blob->offset.y + orig_y,
+                                -blob->offset.x + 10 + orig_x, blob->offset.y + orig_y);
+                        fl_line(-blob->offset.x + orig_x, blob->offset.y - 10 + orig_y,
+                                -blob->offset.x+ orig_x, blob->offset.y + 10 + orig_y);
+                        sprintf(str, "%d", blob->id);
+                        fl_color(FL_YELLOW);
+                        fl_draw(str,-blob->offset.x + orig_x, blob->offset.y + orig_y);
+                        fl_color(FL_BLACK);
+
+                    }
+                    else
+                    {
+                        fl_rect(blob->offset.x -  blob->size.x / 2 + orig_x,
+                                - blob->offset.y - blob->size.y / 2 + orig_y,
+                                blob->size.x,
+                                blob->size.y);
+                        fl_color(FL_WHITE);
+
+                        fl_line(blob->offset.x -10 + orig_x, -blob->offset.y + orig_y,
+                                blob->offset.x + 10 + orig_x, -blob->offset.y + orig_y);
+                        fl_line(blob->offset.x + orig_x, -blob->offset.y - 10 + orig_y,
+                                blob->offset.x+ orig_x, -blob->offset.y + 10 + orig_y);
+                        sprintf(str, "%d", blob->id);
+                        fl_color(FL_YELLOW);
+                        fl_draw(str,blob->offset.x + orig_x, -blob->offset.y + orig_y);
+                        fl_color(FL_BLACK);
+                    }
                 }
             }
         }
@@ -126,23 +137,23 @@ void Canvas::draw_overlay()
             fl_rect((*it)->region_x1,(*it)->region_y1,(*it)->region_x2-(*it)->region_x1,(*it)->region_y2-(*it)->region_y1);
             fl_color(FL_BLACK);
             fl_line_style(FL_SOLID);
-/*
-            int index;
-            int r,g,b,y,u,v;
-            index = ((region_y1 - starty )* width + region_x1-startx )* 3 ;
-            r = img_rgb_buffer[index];
-            g = img_rgb_buffer[index+1];
-            b = img_rgb_buffer[index+2];
-            RGB2YUV(r,g,b,y,u,v);
-            sprintf(str, "(%d %d)- (%d %d %d) (%d %d %d)", region_x1-startx, region_y1-starty, r,g,b,y,u,v);
-            fl_draw(str,100, 20);
-            index = ((region_y2 - starty )* width + region_x2-startx )* 3 ;
-            r = img_rgb_buffer[index];
-            g = img_rgb_buffer[index+1];
-            b = img_rgb_buffer[index+2];
-            RGB2YUV(r,g,b,y,u,v);
-            sprintf(str, "(%d %d)- (%d %d %d) (%d %d %d)", region_x2-startx, region_y2-starty, r,g,b,y,u,v);
-            fl_draw(str,100, 40);*/
+            /*
+               int index;
+               int r,g,b,y,u,v;
+               index = ((region_y1 - starty )* width + region_x1-startx )* 3 ;
+               r = img_rgb_buffer[index];
+               g = img_rgb_buffer[index+1];
+               b = img_rgb_buffer[index+2];
+               RGB2YUV(r,g,b,y,u,v);
+               sprintf(str, "(%d %d)- (%d %d %d) (%d %d %d)", region_x1-startx, region_y1-starty, r,g,b,y,u,v);
+               fl_draw(str,100, 20);
+               index = ((region_y2 - starty )* width + region_x2-startx )* 3 ;
+               r = img_rgb_buffer[index];
+               g = img_rgb_buffer[index+1];
+               b = img_rgb_buffer[index+2];
+               RGB2YUV(r,g,b,y,u,v);
+               sprintf(str, "(%d %d)- (%d %d %d) (%d %d %d)", region_x2-startx, region_y2-starty, r,g,b,y,u,v);
+               fl_draw(str,100, 40);*/
         }
 
         if((*it)->clicked > 0)
@@ -242,7 +253,7 @@ int Canvas::handle(int event)
                                 for(int j= (*it)->region_x1 -  (*it)->startx;j<= (*it)->region_x2- (*it)->startx;j++)
                                 {
                                     ImageBox * imgbox = (*it)->imagebox;
-         
+
                                     if(imgbox)
                                     {
                                         int width = imgbox->img_width;
@@ -266,7 +277,7 @@ int Canvas::handle(int event)
                                 }
                             }
                             fclose(fp);
-                            
+
                             unsigned char *y_result = Quartile(y_buffer, w*h);
                             unsigned char *u_result = Quartile(u_buffer, w*h);
                             unsigned char *v_result = Quartile(v_buffer, w*h);
@@ -306,96 +317,96 @@ int Canvas::handle(int event)
             }
             /*
 
-            if(clicked>2)
-            {
-                clicked = 0;
-            }
+               if(clicked>2)
+               {
+               clicked = 0;
+               }
 
-            else if (clicked == 1)
-            {
-                region_x1 = Fl::event_x();
-                region_y1 = Fl::event_y();
-            }
+               else if (clicked == 1)
+               {
+               region_x1 = Fl::event_x();
+               region_y1 = Fl::event_y();
+               }
             //else if (clicked == 2)
             {
-                printf("%d %d %d %d\n", region_x1 - startx, region_y1 - starty, region_x2 - startx, region_y2 - starty);
-                string time_string;
-                ::time_t time_now = time(NULL);
-                struct tm * timeinfo;
-                timeinfo = localtime (&time_now);
+            printf("%d %d %d %d\n", region_x1 - startx, region_y1 - starty, region_x2 - startx, region_y2 - starty);
+            string time_string;
+            ::time_t time_now = time(NULL);
+            struct tm * timeinfo;
+            timeinfo = localtime (&time_now);
 
-                char filename[64];
-                sprintf(filename, "log-%02d%02d-%02d:%02d:%02d.csv", timeinfo->tm_mon+1, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-*/
-                /*
-                   FILE *fp = fopen(filename, "w");
+            char filename[64];
+            sprintf(filename, "log-%02d%02d-%02d:%02d:%02d.csv", timeinfo->tm_mon+1, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+            */
+            /*
+               FILE *fp = fopen(filename, "w");
 
-                   int r,g,b,y,u,v;
-                   int w = region_x2 - region_x1 +1;
-                   int h = region_y2 - region_y1 + 1;
-                   if(w > 0 && h>0)
-                   {
-                   int index=0;
-                   unsigned char *temp_buffer = new unsigned char[ w * h * 3];
-                   unsigned char * y_buffer = new unsigned char[w*h];
-                   unsigned char * u_buffer = new unsigned char[w*h];
-                   unsigned char * v_buffer = new unsigned char[w*h];
-                   unsigned char * r_buffer = new unsigned char[w*h];
-                   unsigned char * g_buffer = new unsigned char[w*h];
-                   unsigned char * b_buffer = new unsigned char[w*h];
-                   memset(temp_buffer, 0, w * h * 3);
-                   for(int i=region_y1 - starty;i<=region_y2-starty;i++)
-                   {
-                   for(int j=region_x1 - startx;j<=region_x2-startx;j++)
-                   {
-                //        r = img_rgb_buffer[(i* width + j )*3];
-                //        g = img_rgb_buffer[(i* width + j )*3 + 1];
-                //        b = img_rgb_buffer[(i* width + j )*3 + 2];
-                RGB2YUV(r,g,b,y,u,v);
-                //save y,u,v separately
-                y_buffer[index/3] = y;
-                u_buffer[index/3] = u;
-                v_buffer[index/3] = v;
-                r_buffer[index/3] = r;
-                g_buffer[index/3] = g;
-                b_buffer[index/3] = b;
-                //save rgb for drawing
-                temp_buffer[index++] = r;
-                temp_buffer[index++] = g;
-                temp_buffer[index++] = b;
-                fprintf(fp, "%d\t%d\t%d\t%d\t%d\t%d\n", r, g, b, y, u, v);
-                }
-                }
-                fclose(fp);
-                unsigned char *y_result = Quartile(y_buffer, w*h);
-                unsigned char *u_result = Quartile(u_buffer, w*h);
-                unsigned char *v_result = Quartile(v_buffer, w*h);
-                unsigned char *r_result = Quartile(r_buffer, w*h);
-                unsigned char *g_result = Quartile(g_buffer, w*h);
-                unsigned char *b_result = Quartile(b_buffer, w*h);
+               int r,g,b,y,u,v;
+               int w = region_x2 - region_x1 +1;
+               int h = region_y2 - region_y1 + 1;
+               if(w > 0 && h>0)
+               {
+               int index=0;
+               unsigned char *temp_buffer = new unsigned char[ w * h * 3];
+               unsigned char * y_buffer = new unsigned char[w*h];
+               unsigned char * u_buffer = new unsigned char[w*h];
+               unsigned char * v_buffer = new unsigned char[w*h];
+               unsigned char * r_buffer = new unsigned char[w*h];
+               unsigned char * g_buffer = new unsigned char[w*h];
+               unsigned char * b_buffer = new unsigned char[w*h];
+               memset(temp_buffer, 0, w * h * 3);
+               for(int i=region_y1 - starty;i<=region_y2-starty;i++)
+               {
+               for(int j=region_x1 - startx;j<=region_x2-startx;j++)
+               {
+            //        r = img_rgb_buffer[(i* width + j )*3];
+            //        g = img_rgb_buffer[(i* width + j )*3 + 1];
+            //        b = img_rgb_buffer[(i* width + j )*3 + 2];
+            RGB2YUV(r,g,b,y,u,v);
+            //save y,u,v separately
+            y_buffer[index/3] = y;
+            u_buffer[index/3] = u;
+            v_buffer[index/3] = v;
+            r_buffer[index/3] = r;
+            g_buffer[index/3] = g;
+            b_buffer[index/3] = b;
+            //save rgb for drawing
+            temp_buffer[index++] = r;
+            temp_buffer[index++] = g;
+            temp_buffer[index++] = b;
+            fprintf(fp, "%d\t%d\t%d\t%d\t%d\t%d\n", r, g, b, y, u, v);
+            }
+            }
+            fclose(fp);
+            unsigned char *y_result = Quartile(y_buffer, w*h);
+            unsigned char *u_result = Quartile(u_buffer, w*h);
+            unsigned char *v_result = Quartile(v_buffer, w*h);
+            unsigned char *r_result = Quartile(r_buffer, w*h);
+            unsigned char *g_result = Quartile(g_buffer, w*h);
+            unsigned char *b_result = Quartile(b_buffer, w*h);
 
-                //set slider values
-                sliders[RED]->value(r_result[0]);
-                sliders[GREEN]->value(g_result[0]);
-                sliders[BLUE]->value(b_result[0]);
-                sliders[YMIN]->value(y_result[3] < y_result[5] ? y_result[5] : y_result[3]);
-                sliders[YMAX]->value(y_result[4] < y_result[6] ? y_result[4] : y_result[6]);
-                sliders[UMIN]->value(u_result[3] < u_result[5] ? u_result[5] : u_result[3]);
-                sliders[UMAX]->value(u_result[4] < u_result[6] ? u_result[4] : u_result[6]);
-                sliders[VMIN]->value(v_result[3] < v_result[5] ? v_result[5] : v_result[3]);
-                sliders[VMAX]->value(v_result[4] < v_result[6] ? v_result[4] : v_result[6]);
+            //set slider values
+            sliders[RED]->value(r_result[0]);
+            sliders[GREEN]->value(g_result[0]);
+            sliders[BLUE]->value(b_result[0]);
+            sliders[YMIN]->value(y_result[3] < y_result[5] ? y_result[5] : y_result[3]);
+            sliders[YMAX]->value(y_result[4] < y_result[6] ? y_result[4] : y_result[6]);
+            sliders[UMIN]->value(u_result[3] < u_result[5] ? u_result[5] : u_result[3]);
+            sliders[UMAX]->value(u_result[4] < u_result[6] ? u_result[4] : u_result[6]);
+            sliders[VMIN]->value(v_result[3] < v_result[5] ? v_result[5] : v_result[3]);
+            sliders[VMAX]->value(v_result[4] < v_result[6] ? v_result[4] : v_result[6]);
 
-                sprintf(filename, "log-%02d%02d-%02d:%02d:%02d.ppm", timeinfo->tm_mon, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-                ppm16Write(filename,temp_buffer, w, h);
-                delete []y_result;
-                delete []u_result;
-                delete []v_result;
-                delete []r_result;
-                delete []g_result;
-                delete []b_result;
-                delete []temp_buffer;
-                }
-                */
+            sprintf(filename, "log-%02d%02d-%02d:%02d:%02d.ppm", timeinfo->tm_mon, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+            ppm16Write(filename,temp_buffer, w, h);
+            delete []y_result;
+            delete []u_result;
+            delete []v_result;
+            delete []r_result;
+            delete []g_result;
+            delete []b_result;
+            delete []temp_buffer;
+            }
+            */
 
             //}
             break;
@@ -457,8 +468,12 @@ bool Canvas::AttachToiDisplay(ImageBox* imgbox)
         imageboxes.push_back(imgbox);
 
     iDisplay * display = GetNextiDisplay();
+
     if(!display)
+    {
         return false;
+    }
     else
-        display->SetImageBox(imgbox);
+        return display->SetImageBox(imgbox);
+
 }
